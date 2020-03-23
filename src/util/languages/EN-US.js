05d8437a2e427cc7eed474fa6410093ebb7f792a
UndecidedFactor <53883918+UndecidedSource@users.noreply.github.com>
2020-03-23 12:52:41 -0400

    Update EN-US.js

--- src/util/languages/EN-US.js
+++ src/util/languages/EN-US.js
@@ -3,4 +3,8 @@ module.exports = {
     "COMMAND_8BALL_DESCRIPTION": "Ask any question to the magic 8ball",
     "COMMAND_8BALL_USAGE": "t!8ball <question>",
     "COMMAND_8BALL_MISSING_ARGS": "You must provide a valid question to ask the 8ball",
+    // ban
+    "COMMAND_BAN_DESCRIPTION": "Bans the specified user",
+    "COMMAND_BAN_USAGE": "t!ban <user> [reason]",
+    "COMMAND_BAN_USER_BANNED": ""
 }
