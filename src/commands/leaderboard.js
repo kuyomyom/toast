8bf829ce773c8c76fe88eb77da0b6d267902a11c
UndecidedFactor <53883918+UndecidedSource@users.noreply.github.com>
2020-03-27 13:49:26 -0400

    Update leaderboard.js

--- src/commands/leaderboard.js
+++ src/commands/leaderboard.js
@@ -6,11 +6,13 @@ module.exports.run = async (client, message, args, { guild, user, error }) => {
 	try {
 		if (!guild.levelingactive) return message.channel.send("Leveling is not enabled for this server.");
 
+		await guild.fetchMembers();
+
 		text = "";
 		Array.from(guild.leveling.values())
     		.sort((a, b) => b[1].level - a[1].level)
    			.splice(0, 9)
-    		.map(i => `• **${client.users.cache.get(i[0]).tag}**\n\tLevel: ${(i[1].level).toString()}\n\tXP: ${(i[1].xp).toString()}`)
+    		.map(i => `• **${client.users.get(i[0]).tag}**\n\tLevel: ${(i[1].level).toString()}\n\tXP: ${(i[1].xp).toString()}`)
 				 .forEach(i => text += i + "\n")
 
 		const embed = new RichEmbed()
