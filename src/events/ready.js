50f875c5cc094ea8a684de08bfb48b410236453d
UndecidedFactor <53883918+UndecidedSource@users.noreply.github.com>
2020-03-28 21:59:25 -0400

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,8 +1,13 @@
-const { RichEmbed } = require('discord.js');
+const DBL = require("dblapi.js");
+const { config } = require("dotenv");
+const { RichEmbed, Client } = require('discord.js');
 const { Info, Logging } = require('../config');
+
+// const dbl = new DBL(process.env.DBL_TOKEN, Client);
 
 module.exports = async (client) => {
 
+	const totalGuilds = await client.shard.fetchClientValues('guilds.size');
 	const username = Info.NAME;
 	const startChannel = Logging.LOG_CHANNEL;
 
@@ -11,6 +16,11 @@ module.exports = async (client) => {
 		type: 'STREAMING',
 		url: 'https://twitch.tv/monster',
 	});
+
+	/*dbl.postStats(totalGuilds, client.shards.Id, client.shards.total);
+	setInterval(() => {
+        dbl.postStats(totalGuilds, client.shards.Id, client.shards.total);
+    }, 1800000);*/
 
 	const readyEmbed = new RichEmbed()
 		.setTitle(`${username}: Process Started`)
